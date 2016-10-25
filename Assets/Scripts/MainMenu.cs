fe188b270f9b1ab77c1790be61deb8134c6c38d3
OHoncharov <asimplegames@gmail.com>
2016-10-25 20:28:35 +0300

    Added csv reader

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnClick () {
+		var csv = GameObject.Find ("CSVReader").GetComponent<CSVReader> ().SplitCsvGrid("");
+		Debug.Log (csv.Length);
+	}
+}
