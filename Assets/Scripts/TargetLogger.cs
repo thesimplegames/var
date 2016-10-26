0f77a968a3a6abb886d33fa2e623c6e2bd85f5bf
OHoncharov <asimplegames@gmail.com>
2016-10-26 22:34:14 +0300

    Added TargetLogger

---
+++ Assets/Scripts/TargetLogger.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TargetLogger : MonoBehaviour {
+
+    public static TargetLogger Instance;
+
+
+    public List<string> targets;
+    public string output = "";
+
+    void Enable ()
+    {
+        targets = new List<string>();
+    }
+
+	// Use this for initialization
+	void Start () {
+        Instance = this;
+	}
+	
+	// Update is called once per frame
+	void OnGUI () {
+        GUI.Button(new Rect(Screen.width - 150, 0, 150, Screen.height), "");
+	}
+}
---
+++ Assets/Scripts/TrackingDetector.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using Vuforia;
+
+public class TrackingDetector : MonoBehaviour,
+                                            ITrackableEventHandler
+{
+    private TrackableBehaviour mTrackableBehaviour;
+
+    bool showGUI = false;
+
+    string targetName;
+
+    void Start()
+    {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+        {
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+
+        targetName = GetComponent<ImageTargetBehaviour>().TrackableName;
+        
+    }
+
+    public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            showGUI = true;
+            //throw new System.Exception("ASD");
+
+            if (!TargetLogger.Instance.targets.Contains(targetName))
+            {
+                TargetLogger.Instance.targets.Add(targetName);
+                TargetLogger.Instance.output += targetName + "\n";
+            }
+        }
+        else
+        {
+            showGUI = false;
+        }
+    }
+
+    void OnGUI ()
+    {
+        if (showGUI == false)
+            return;
+
+        GUI.Button(new Rect(0, 0, 200, 100), targetName);
+    }
+}
